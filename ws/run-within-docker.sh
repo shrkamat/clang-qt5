d6d9518a1eb5d342a6240bb69bc913838c01819d
Shrinivas Kamath <shrkamat@cisco.com>
2019-08-10 12:23:50 +0530

    minor changes

--- ws/run-within-docker.sh
+++ ws/run-within-docker.sh
@@ -15,7 +15,7 @@ CROOT=$PWD
 # clang
 cd $CROOT
 mkdir -p clang/build/llvm && cd clang/build/llvm
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD='X86' -DLLVM_ENABLE_PROJECTS='clang' -DCOMPILER_RT_BUILD_SANITIZERS=OFF -G Ninja $CROOT/clang/code/llvm
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD='X86' -DLLVM_ENABLE_PROJECTS='clang' -DCOMPILER_RT_BUILD_SANITIZERS=OFF -DLLVM_BUILD_RUNTIME=false -G Ninja $CROOT/clang/code/llvm
 ninja install
 
 # libcxx round 1
@@ -29,9 +29,9 @@ ninja install
 cd $CROOT
 cd $CROOT/clang/code/llvm/libcxxabi/lib
 ./buildit
-cp libc++abi.so.1.0 /usr/lib/
-ln -s /usr/lib/libc++abi.so.1.0 /usr/lib/libc++abi.so.1
-ln -s /usr/lib/libc++abi.so.1.0 /usr/lib/libc++abi.so
+cp -f libc++abi.so.1.0 /usr/lib/
+ln -sf /usr/lib/libc++abi.so.1.0 /usr/lib/libc++abi.so.1
+ln -sf /usr/lib/libc++abi.so.1.0 /usr/lib/libc++abi.so
 
 # libcxx round 2 (this time with newly generated libcxxabi)
 cd $CROOT
@@ -39,3 +39,9 @@ mkdir -p clang/build/libcxx-2 && cd clang/build/libcxx-2
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLIBCXX_CXX_ABI=libcxxabi -DLIBCXX_LIBCXXABI_INCLUDE_PATHS=$CROOT/clang/code/llvm/libcxxabi/include -G Ninja $CROOT/clang/code/llvm/libcxx
 ninja install
 
+
+# # compiler-rt
+# cd $CROOT
+# mkdir -p clang/build/compiler-rt && cd clang/build/compiler-rt
+# cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DLLVM_BUILD_RUNTIME=ON $CROOT/clang/code/llvm/projects
+# ninja
